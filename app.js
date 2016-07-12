32e6c60d04a846c9078ae7a79be54e399e183b0c
Aleksandra <aleksa000777@mail.ru>
2016-07-12 12:40:20 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const cssnext = require('postcss-cssnext')
+const sugarss = require('sugarss')
+const es2015 = require('babel-preset-es2015')
+const stage2 = require('babel-preset-stage-2')
+const cssnano = require('cssnano')
+
+
+
+module.exports = {
+  postcss: {
+    plugins: [cssnext, cssnano],
+    parser: sugarss
+  },
+  babel: { presets: [es2015, stage2] },
+  ignore: ['**/layout.jade', '**/_*', '**/.*', 'readme.md', 'views/_includes']
+}
